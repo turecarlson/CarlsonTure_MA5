b979838da9a5ca168454fd5b46ae37ef84539fd9
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2021-03-03 11:25:39 -0500

    defined page title in public/index.html

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import './App.css';
 export default class App extends React.Component {
 
     render() {
-        document.title = 'MA5 - Ture Carlson'; //Set the page title in the document
         return (
             <div className='app'>
                 <ParentCell />
